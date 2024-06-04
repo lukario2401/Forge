74d4d3c8dd6d2b116d41ba0f86e8f37d2062d728
lukario2401 <lukadolidze433@gmail.com>
2024-06-04 19:22:55 +0400

    Changed the class to Ll

---
+++ src/main/java/net/luka/ll/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.luka.ll.item;
+
+import net.luka.ll.Ll;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Ll.MOD_ID);
+
+
+    public static final RegistryObject<Item> zircon = ITEMS.register("zircon",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
