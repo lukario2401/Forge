225a324ec9ce661ba7b84621b54aa20c5c57b952
lukario2401 <lukadolidze433@gmail.com>
2024-06-08 00:43:39 +0400

    8 ball added

---
+++ src/main/java/net/luka/ll/item/custom/EightBallItem.java
@@ -0,0 +1,11 @@
+package net.luka.ll.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class EightBallItem extends Item {
+
+    public EightBallItem(Properties properties) {
+        super(properties);
+    }
+
+}
