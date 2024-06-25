360b851a53b276e162f94ee8aa52241fc4df7594
lukario2401 <lukadolidze433@gmail.com>
2024-06-25 14:53:57 +0400

    added key binds

---
+++ src/main/java/net/luka/ll/event/ClientEvents.java
@@ -0,0 +1,32 @@
+package net.luka.ll.event;
+
+import net.luka.ll.Ll;
+import net.luka.ll.util.KeyBinding;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.vehicle.Minecart;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+public class ClientEvents {
+    @Mod.EventBusSubscriber(modid = Ll.MOD_ID, value = Dist.CLIENT)
+    public static class ClientForgeEvents {
+        @SubscribeEvent
+        public static void onKeyInput(InputEvent.Key event) {
+            if(KeyBinding.DRINKING_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Pressed a Key!"));
+            }
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = Ll.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModBusEvents {
+        @SubscribeEvent
+        public static void onKeyRegister(RegisterKeyMappingsEvent event) {
+            event.register(KeyBinding.DRINKING_KEY);
+        }
+    }
+}
---
+++ src/main/java/net/luka/ll/util/KeyBinding.java
@@ -0,0 +1,15 @@
+package net.luka.ll.util;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import org.lwjgl.glfw.GLFW;
+
+public class KeyBinding {
+    public static final String KEY_CATEGORY_LL = "key.category.ll.lll";
+    public static final String KEY_DRINK_WATER = "key.ll.dink_water";
+
+
+    public static final KeyMapping DRINKING_KEY = new KeyMapping(KEY_DRINK_WATER, KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_O,KEY_CATEGORY_LL);
+}
